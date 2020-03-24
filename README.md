b7b9efb3ccdcf0d3adaf50ad41e577b4d2ef1362
jerry lee <jerrylee@jerrys-MacBook-Pro.local>
2020-03-24 12:52:56 -0500

    added notes in readme.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TensorFlow
+
+# install Tensor flow into your mac 
+#pip install tensorflow
+pip3 install tensorflow
---
+++ hello_tf.py
@@ -0,0 +1,12 @@
+""" A simple TensorFlow application """
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+msg = tf.string__join(['hello','Tensor'])
+
+with tf.session() as session:
+
