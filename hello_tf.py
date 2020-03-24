eb31e3c767066ed56ffc270b58c266c2333924d3
jerry lee <jerrylee@jerrys-MacBook-Pro.local>
2020-03-24 13:43:21 -0500

    change hello script to older version of tf

--- hello_tf.py
+++ hello_tf.py
@@ -4,9 +4,17 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+# import tensorflow as tf
 
-msg = tf.string__join(['hello','Tensor'])
+# msg = tf.string__join(['hello','Tensor'])
+msg = tf.strings.join(['Hello ', 'TensorFlow'])
 
-with tf.session() as session:
+with tf.Session() as sess:
+    print(sess.run(msg))
+    print(sess.run(msg))
+
+# with tf.compat.v1.Session() as sess:
+#         print(sess.run(msg))
 
